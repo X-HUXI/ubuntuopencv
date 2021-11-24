596d4b0e6ee5e7e2deb0e0cf4b287d7a5b065a09
X-HUXI <82788537+X-HUXI@users.noreply.github.com>
2021-11-24 23:09:05 +0800

    Add files via upload

---
+++ C#/window/myWindowsFormsApp/myWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace myWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(29, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(592, 297);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(29, 373);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(592, 25);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(676, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "客户端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = "The current time: ";
+            str += DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            textBox1.AppendText(str + Environment.NewLine);
+            UdpClient udpSender = new UdpClient(0);
+            int port = 8000;
+            string host = "192.168.43.41";//我室友的IP地址
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);//把ip和端口转化为IPEndPoint实例
+            udpSender.Connect(host, port);
+            string message = textBox2.Text;
+            byte[] sendBytes = Encoding.UTF8.GetBytes(message);
+            udpSender.Send(sendBytes, sendBytes.Length);
+            string sendStr = textBox2.Text;
+            str = "The message content: " + sendStr;
+            textBox1.AppendText(str + Environment.NewLine);
+            str = "Send the message to the server...";
+            textBox1.AppendText(str + Environment.NewLine);
+            byte[] recvStr = udpSender.Receive(ref ipe);
+            string message1 = Encoding.UTF8.GetString(recvStr, 0, recvStr.Length);
+            str = "The server feedback: " + message1;//显示服务器返回信息
+            textBox1.AppendText(str + Environment.NewLine);
+
+        }
+    }
+}
+
